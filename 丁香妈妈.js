b1f0c08bd74690e89706e93a0c03c3ef7ea2944e
朱峰 <386408003@qq.com>
2021-12-08 11:07:54 +0800

    1.优化重构丁香妈妈打卡代码。

--- 丁香妈妈.js
+++ 丁香妈妈.js
@@ -1,27 +1,35 @@
-/* --------版本 v0.03 根据个人情况修改以下内容---------- */
+/* --------版本 v0.05 根据个人情况修改以下内容---------- */
 
 // 使用本地存储
 var storage = storages.create("386408003@qq.com:config");
 // 引入工具组件
 var utils = require(storage.get("rootPath") + "utils/utils.js");
+// 解锁屏幕
+utils.unlock(storage.get("password"));
 
 // 设置屏幕常亮时间，默认 45 分钟
 const SCREEN_DIM_TIME = 45 * 60 * 1000;
 // 播放课程时间，默认 5.5 分钟
 const PLAY_COURSE_TIME = 5 * 60 * 1000 + 30 * 1000;
-// 长等待时间常量，用于应用启动、广告等较长时间等待，如果网络不好或手机卡请增加此数值，默认 6 秒
+// 长等待时间常量，用于应用启动等较长时间等待，如果网络不好或手机卡请增加此数值，默认 5 秒
 const LONG_TIME = 5000;
-// 短等待时间常量，用于例如返回等每步操作后的等待，如果手机卡请增加此数值，默认 3 秒
+// 短等待时间常量，用于按钮点击、返回等每步操作后的等待，如果网络不好或手机卡请增加此数值，默认 3 秒
 const SHORT_TIME = 3000;
 // 更短的等待时间，没有请求时的短暂等待，默认 500 毫秒
 const NANO_TIME = 500;
-// 打卡是否完成标志，多少课程就写多少
-var FINISH_MARK = 6;
-
-// 我的打卡按钮位置
-const point1 = { x: 150, y: 830 };
-// 播放按钮位置
-const point2 = { x: 150, y: 1080 };
+// 组件找不到重试次数，默认 5 次
+const maxRetryTimes = 5;
+// 所有打卡的课程
+const allCourseName = [
+  ["0~6 岁绘本早教馆"],
+  ["百科认知童谣", "海浪"],
+  ["动物认知童谣", "猎豹"],
+  ["生活认知童谣", "厨房  "],
+  ["身体认知童谣", "头发 "],
+  ["食物认知童谣", "强壮猪肝", "红肉"]
+];
+// 根据此标志判断打卡是否完成
+var FINISH_MARK = allCourseName.length;
 
 // 没有无障碍时候会提示无障碍模式的开启，并且开启之后,会接着继续运行。
 auto.waitFor();
@@ -37,19 +45,16 @@ setFloatWindow();
 keyDetector();
 // 保持屏幕常亮，默认 35 分钟
 device.keepScreenDim(SCREEN_DIM_TIME);
-// 解锁屏幕
-utils.unlock(storage.get("password"));
 
-/* --------版本 v0.03 根据个人情况修改以上内容---------- */
-
+/* --------版本 v0.05 根据个人情况修改以上内容---------- */
 
 // 开始打卡
-watchVideo();
+startClockIn();
 
 /**
  * 打卡主方法
  */
-function watchVideo() {
+function startClockIn() {
   launchApp("丁香妈妈");
   sleep(LONG_TIME);
 
@@ -59,56 +64,69 @@ function watchVideo() {
   }
 
   // 点击导航栏 课程 一栏
-  let main_course = id("main_course").findOne(SHORT_TIME);
-  if (main_course != null) {
-    main_course.click();
-    sleep(SHORT_TIME);
-    // 按照位置点击我的打卡
-    click(point1.x, point1.y);
+  if (findUntilClick(id("com.dxy.gaia:id/main_course"))) {
+    // 计算我的打卡的位置进行点击
+    let myClockBounds = id("com.dxy.gaia:id/home_hot_zone_image_container").findOnce().bounds();
+    let myClockX = myClockBounds.left + (myClockBounds.right - myClockBounds.left) / 10;
+    let myClockY = myClockBounds.top + (myClockBounds.bottom - myClockBounds.top) / 4;
+    utils.toast_console("我的打卡计算位置为：" + myClockX + ", " + myClockY);
+    click(myClockX, myClockY);
     sleep(SHORT_TIME);
   } else {
     utils.toast_console("未找到课程，请检查网络！", true);
   }
 
   // 点击 全部课程 按钮
-  let allCourse = text("全部课程").findOnce();
-  if (allCourse != null) {
-    allCourse.click();
-    sleep(SHORT_TIME);
+  if (findUntilClick(text("全部课程"))) {
     // 开始打卡
-    playCourse("0~6 岁绘本早教馆");
-    playCourse("百科认知童谣", "海浪");
-    playCourse("动物认知童谣", "猎豹");
-    playCourse("生活认知童谣", "厨房  ");
-    playCourse("身体认知童谣", "头发 ");
-    playCourse("食物认知童谣", "强壮猪肝", "红肉");
+    for (let i = 0; i < allCourseName.length; i++) {
+      if (playCourse(allCourseName[i])) {
+        utils.toast_console(allCourseName[i][0] + "课程打卡完毕！");
+      } else {
+        utils.toast_console(allCourseName[i][0] + "课程打卡失败，请检查课程名称！");
+      }
+    }
   } else {
     utils.toast_console("未找到全部课程，请确认是否登陆！", true);
   }
+
   // 打卡成功
   if (FINISH_MARK == 0) {
     utils.toast_console("所有课程已打卡完毕！", true);
-    utils.sendMail("丁香妈妈", "绘本课程打卡完成！\n其他5门课程都已打卡完成！", "[打卡完成]");
+    utils.sendMail("丁香妈妈", "所有课程都已打卡完成！", "[打卡完成]");
     sleep(SHORT_TIME);
+  } else {
+    utils.toast_console("未打卡完毕！");
   }
   closeApp();
 }
 
 /**
  * 播放课程
- * @param {string} courseType 课程类型
- * @param {string} courseName 课程名字
- * @param {string} parentName 课程上级菜单名字
+ * @param {Array} courseNameArray [课程类型, 课程名字, 课程上级菜单名字]
  * @returns 
  */
-function playCourse(courseType, courseName, parentName) {
-  utils.toast_console(courseType + "：开始打卡");
-  bean = text(courseType).findOnce();
-  if (bean.parent().parent().parent().child(5).text() == "今日已打卡") {
-    utils.toast_console(courseType + "：今日已打卡");
-    return FINISH_MARK--;
+function playCourse(courseNameArray) {
+  let [courseType, courseName, parentName] = courseNameArray;
+  utils.toast_console(courseType + "：开始打卡！");
+  let component = text(courseType).findOnce();
+  // 课程名字错误导致打卡失败
+  if (component == null) {
+    return false;
   }
-  bean.click();
+  // 判断本次是否已打卡
+  if (component.parent().parent().parent().child(5).text() == "今日已打卡") {
+    FINISH_MARK--
+    utils.toast_console(courseType + "：今日已打卡！");
+    return true;
+  }
+  // 判断课程是否已全部打卡完成
+  if (component.parent().parent().parent().child(5).text() == "已完成") {
+    FINISH_MARK--
+    utils.toast_console(courseType + "：打卡已完成，无需打卡！");
+    return true;
+  }
+  component.click();
   sleep(SHORT_TIME);
   // courseName 没有值，即传了一个参数
   if (!courseName) {
@@ -116,30 +134,63 @@ function playCourse(courseType, courseName, parentName) {
     let px = id("com.dxy.gaia:id/story_book_image_item").findOnce().bounds().centerX();
     let py = id("com.dxy.gaia:id/story_book_image_item").findOnce().bounds().centerY();
     click(px, py);
+    sleep(SHORT_TIME);
+    // 关闭首次操作提示框
+    while (id("com.dxy.gaia:id/tv_skip").exists()) {
+      id("com.dxy.gaia:id/tv_skip").findOnce().click();
+    }
     sleep(PLAY_COURSE_TIME * 3);
-    utils.toast_console(courseType + "：打卡结束");
+    FINISH_MARK--
+    utils.toast_console(courseType + "：打卡结束！");
     // 返回选择课程页面
     switchCourse();
-    return FINISH_MARK--;
+    return true;
   }
-  swipe(500, 1000, 500, 3000, NANO_TIME);
-  sleep(NANO_TIME);
-  if (!text(courseName).exists()) {
-    let px = text(parentName).findOnce().bounds().centerX();
-    let py = text(parentName).findOnce().bounds().centerY();
-    click(px, py);
-    sleep(SHORT_TIME);
+
+  let maxSwipeRetryTimes = maxRetryTimes;
+  // parentName 没有值，说明没有上下级课程关系
+  if (!parentName) {
+    // 尝试查找课程名字是否存在，不存在就下拉
+    while (!text(courseName).exists() && maxSwipeRetryTimes--) {
+      swipe(width / 2, height / 2, width / 2, height, NANO_TIME);
+      sleep(NANO_TIME);
+      utils.toast_console("下拉 " + (maxRetryTimes - maxSwipeRetryTimes) + " 次，再次查找课程！");
+    }
+  } else {
+    maxSwipeRetryTimes = maxSwipeRetryTimes + 1;
+    // 尝试查找课程名字是否存在
+    while (!text(courseName).exists() && maxSwipeRetryTimes--) {
+      // 判断父级是否存在
+      while (!text(parentName).exists() && maxSwipeRetryTimes--) {
+        swipe(width / 2, height / 2, width / 2, height, NANO_TIME);
+        sleep(NANO_TIME);
+        utils.toast_console("下拉 " + (maxRetryTimes - maxSwipeRetryTimes) + " 次，再次查找课程！");
+      }
+      if (!text(courseName).exists()) {
+        let px = text(parentName).findOnce().bounds().centerX();
+        let py = text(parentName).findOnce().bounds().centerY();
+        click(px, py);
+        sleep(SHORT_TIME);
+      }
+    }
   }
+  // 点击找到的课程
   let ax = text(courseName).findOnce().bounds().centerX();
   let ay = text(courseName).findOnce().bounds().centerY();
   click(ax, ay);
   sleep(SHORT_TIME);
-  click(point2.x, point2.y);
+  // 点击播放按钮
+  let playButtonBounds = packageName("com.dxy.gaia").className("android.view.View").clickable(false).depth(10).indexInParent(1).findOnce().bounds();
+  let playButtonX = playButtonBounds.left + (playButtonBounds.right - playButtonBounds.left) / 8;
+  let playButtonY = playButtonBounds.bottom - (playButtonBounds.bottom - playButtonBounds.top) / 8;
+  utils.toast_console("播放按钮计算位置为：" + playButtonX + ", " + playButtonY);
+  click(playButtonX, playButtonY);
   sleep(PLAY_COURSE_TIME);
   FINISH_MARK--;
-  utils.toast_console(courseType + "：打卡结束");
+  utils.toast_console(courseType + "：打卡结束！");
   // 返回选择课程页面
   switchCourse();
+  return true;
 }
 
 /**
@@ -147,9 +198,29 @@ function playCourse(courseType, courseName, parentName) {
  */
 function switchCourse() {
   back();
-  sleep(SHORT_TIME / 2);
+  sleep(SHORT_TIME);
   back();
-  sleep(SHORT_TIME / 2);
+  sleep(SHORT_TIME);
+}
+
+/**
+ * 查找组件直到找到或者超过重试次数，然后进行点击
+ * @param {Component} component 需要点击的组件
+ * @returns 查找结果
+ */
+function findUntilClick(component) {
+  let maxFindRetryTimes = maxRetryTimes;
+  while (!component.exists() && maxFindRetryTimes--) {
+    sleep(SHORT_TIME);
+    utils.toast_console("第 " + (maxRetryTimes - maxFindRetryTimes) + " 次查找 " + component + " 组件！");
+  }
+  if (component.exists()) {
+    component.findOnce().click();
+    sleep(SHORT_TIME);
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /**
@@ -178,16 +249,16 @@ function setFloatWindow() {
     </frame>
   );
   // 悬浮窗不会自动关闭
-  setInterval(() => { }, 1000);
-  win.setPosition(700, height / 4);
-  win.setSize(400, 300);
+  setInterval(() => { }, NANO_TIME * 2);
+  win.setPosition(width / 3 * 2, height / 4);
+  win.setSize(400, 280);
   // 悬浮窗可调整大小
   win.setAdjustEnabled(true);
   // 长按悬浮窗内文字结束脚本
   win.text.longClick(() => {
     // try...catch把exit()函数的异常捕捉，则脚本不会立即停止，仍会运行几行后再停止
     try {
-      utils.toast_console("检测到长按悬浮窗文字，脚本终止。", true);
+      utils.toast_console("检测到长按悬浮窗文字，脚本终止！", true);
       closeScript();
     } catch (err) { }
     // 直接exit()的话坚持不到return的时候
@@ -205,7 +276,7 @@ function keyDetector() {
     events.on("key", function (code, event) {
       var keyCodeStr = event.keyCodeToString(code);
       if (keyCodeStr == "KEYCODE_VOLUME_DOWN") {
-        utils.toast_console("检测到音量下键，脚本终止。", true);
+        utils.toast_console("检测到音量下键，脚本终止！", true);
         closeScript();
       }
     });
@@ -231,10 +302,10 @@ function closeApp() {
     home();
     sleep(NANO_TIME);
   } else {
-    utils.toast_console(appName + "应用不能被正常关闭或不在后台运行。");
+    utils.toast_console(appName + "应用不能被正常关闭或不在后台运行！");
     home();
   }
-  utils.toast_console("打卡结束，" + appName + "应用已被关闭。", true);
+  utils.toast_console("打卡结束，" + appName + "应用已被关闭！", true);
   closeScript();
 }
 
